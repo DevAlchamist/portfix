6bb5f3b0aba556e0c1c0d6649ea21125dba788a1
DevAlchamist <nayakshubhanshu060705@gmail.com>
2024-04-11 11:48:42 +0530

    Images Done

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,34 +6,34 @@ import next from "@/assets/icons/next-16-svgrepo-com.svg";
 import react from "@/assets/icons/react-svgrepo-com.svg";
 import css from "@/assets/icons/scss-svgrepo-com.svg";
 import redux from "@/assets/icons/redux-svgrepo-com.svg";
-import movix from "@/assets/icons/cine-verse-app.png";
+import movix from "@/assets/Cine-Verse1.jpeg";
+import stripe from "@/assets/icons8-stripe.svg";
 import convex from "@/assets/icons/Logo-2048-white.png";
+import shop1 from "@/assets/Shop-Pulse1.jpeg";
 import ts from "@/assets/icons/typescript.svg";
 import tail from "@/assets/icons/Tailwind CSS.svg";
 import Live from "@/assets/icons/live-location-svgrepo-com.svg";
 import Task from "@/assets/icons/task-mate-app.png";
 import { Cursor, Typewriter } from "react-simple-typewriter";
 
-const page = () => {
+const Page = () => {
   const Images = [
     {
-      image: movix,
+      image: shop1,
       alt: "pr1",
-      translate: "0",
-      name: "Cine-Verse",
+      name: "Shop-Pulse",
       date: "2 Jan, 2024",
       desc: "Cine-Verse is an online platform that serves as a comprehensive database for film and television content, offering detailed information on movies, TV shows, cast, production crew, ratings, genres, and related videos. It encompasses a wide range of entertainment services, including digital cinema, streaming channels, content marketing, and distribution, catering to a broad audience interested in visual entertainment media.",
       tech: [
         { img: react, title: "React" },
-        { img: css, title: "SCSS" },
+        { img: tail, title: "Tailwind" },
         { img: redux, title: "Redux" },
+        { img: stripe, title: "Stripe" },
       ],
     },
     {
       image: Task,
       alt: "pr2",
-      translate: "0",
-      height: "",
       name: "Task-Mate",
       desc: "Task-Mate is a real-time collaboration platform built with Next.js, TypeScript, and Convex Clerk, enabling users to exchange tasks and create organizations in real-time. It features a freehand drawing library, allowing for dynamic and interactive task management. Users can invite others, assign roles, and engage in collaborative workspaces, facilitating efficient project management and teamwork.",
       date: "12 Feb, 2024",
@@ -45,9 +45,22 @@ const page = () => {
         { img: Live, title: "LiveBlocks" },
       ],
     },
+    {
+      image: movix,
+      alt: "pr1",
+      name: "Cine-Verse",
+      date: "2 Jan, 2024",
+      desc: "Cine-Verse is an online platform that serves as a comprehensive database for film and television content, offering detailed information on movies, TV shows, cast, production crew, ratings, genres, and related videos. It encompasses a wide range of entertainment services, including digital cinema, streaming channels, content marketing, and distribution, catering to a broad audience interested in visual entertainment media.",
+      tech: [
+        { img: react, title: "React" },
+        { img: css, title: "SCSS" },
+        { img: redux, title: "Redux" },
+      ],
+    },
   ];
+
   return (
-    <Box className=" bg-[#1e1842] h-full">
+    <Box className="bg-[#1e1842] h-full">
       <Box>
         <Box className="flex items-center justify-center px-5 lg:px-10 py-20">
           <Box className="w-full lg:w-[80%]">
@@ -68,35 +81,42 @@ const page = () => {
                 {Images.map((img) => (
                   <Box key={img.name}>
                     <Box className="col-span-1 grid grid-cols-8 justify-start items-start gap-10">
-                      <Box className="relative lg:col-span-4 col-span-8 shadow-2xl overflow-hidden h-fit lg:h-[400px] rounded-xl ">
-                        <Box className="w-full z-10 h-full absolute inset-0  hover:bg-gradient-to-t text-transparent hover:text-white hover:from-transparent/90 hover:to-transparent">
-                          <Box className="absolute z-20  left-1 bottom-1 text-xl font-bold rounded-lg  py-1 px-5 ">
-                            Visit
-                          </Box>
-                        </Box>
+                      <Box className="relative lg:col-span-4 col-span-8 shadow-2xl overflow-hidden h-fit lg:h rounded-xl">
                         <Image
                           src={img.image}
                           alt={img.alt}
-                          className={`w-full z-0 h-${img.height} transform hover:translate-y-${img.translate} duration-1000 ease-linear`}
+                          className={`w-full z-0 h-fit  duration-1000 ease-linear`}
                         />
-                        <Box className="absolute right-1 top-1 bg-red-500 rounded-lg text-white py-1 px-5 ">
+                        <Box
+                          className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-300 ease-in-out bg-black bg-opacity-50 hover:opacity-100"
+                          style={{ position: "absolute" }}
+                        >
+                          <Box className="text-white font-semibold text-lg p-3">
+                            {img.name}
+                          </Box>
+                        </Box>
+                        <Box className="absolute right-1 top-1 bg-red-500 rounded-lg text-white py-1 px-5">
                           Web
                         </Box>
                       </Box>
                       <Box className="text-white col-span-8 lg:col-span-4">
-                        <Box className="text-xs lg:text-base font-normal">{img.date}</Box>
-                        <Box className="text-xl lg:text-4xl font-semibold">{img.name}</Box>
+                        <Box className="text-xs lg:text-base font-normal">
+                          {img.date}
+                        </Box>
+                        <Box className="text-xl lg:text-4xl font-semibold">
+                          {img.name}
+                        </Box>
                         <Box className="text-sm lg:text-xl w-fit font-normal">
                           {img.desc}
                         </Box>
                         <Box className="text-2xl mt-3 font-semibold">
                           Tech-Stack
                         </Box>
-                        <Box className="flex gap-3 ">
+                        <Box className="flex gap-3">
                           {img.tech?.map((tech) => (
                             <Box
                               key={tech.title}
-                              className=" flex items-center flex-col"
+                              className="flex items-center flex-col group"
                             >
                               <Image
                                 src={tech.img}
@@ -124,4 +144,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,25 +1,35 @@
 "use client";
 import { Box } from "@mui/material";
 import Image from "next/image";
+import shop1 from "@/assets/Shop-Pulse1.jpeg";
+import shop2 from "@/assets/Shop-Pulse2.jpeg";
+import shop3 from "@/assets/Shop-Pulse3.jpeg";
+import shop4 from "@/assets/Shop-Pulse4.jpeg";
 import React from "react";
-import movix from "@/assets/icons/cine-verse-app.png";
+import movix from "@/assets/Cine-Verse1.jpeg";
 import Task from "@/assets/icons/task-mate-app.png";
 import { Cursor, Typewriter } from "react-simple-typewriter";
-
+import "../app/globals.css";
 const Projects = () => {
   const Images = [
     {
-      image: movix,
+      image: [shop1, shop2, shop3, shop4],
+      alt: "pr0",
+      name: "Shop-Pulse",
+    },
+    {
+      image: [movix],
       alt: "pr1",
-      height: "full",
-      translate: "custom",
       name: "Cine-Verse",
     },
     {
-      image: Task,
+      image: [Task],
       alt: "pr2",
-      translate: "0",
       name: "Task-Mate",
+    },
+    {
+      alt: "none",
+      name: "More",
     },
   ];
 
@@ -39,36 +49,39 @@ const Projects = () => {
         </Box>
         <Box className="grid-cols-2 grid gap-5">
           {Images.map((img) => (
-            <Box
-              key={img.alt}
-              className="relative col-span-2 lg:col-span-1 border-white border-2 w-full overflow-hidden h-fit lg:h-[430px] rounded-xl"
-            >
+            <>
               <Box
-                className={`w-full h-${img.height} 
-                ${
-                  img.translate === "custom"
-                    ? "transform transition-transform duration-1000 ease-in-out hover:translate-y-[-80%]"
-                    : "object-fill"
-                }`}
+                key={img.alt}
+                className="relative col-span-2 lg:col-span-1  w-full overflow-hidden h-fit lg:h-[343px] rounded-xl"
+                style={{ position: "relative" }}
               >
-                <Image
-                  src={img.image}
-                  alt={img.alt}
-                  className=" z-0"
-                />
-              </Box>
-              <Box
-                id={`image-${img.alt}`}
-                className="hover:flex hidden hover:bg-gradient-to-t hover:from-gray-600 hover:to-transparent absolute inset-0"
-              >
-                <Box className="absolute left-1 bottom-1 text-4xl font-bold rounded-lg text-white py-1 px-5">
-                  {img.name}
+                <Box className="z-0">
+                  {img.image && (
+                    <Image
+                      src={img.image[0]}
+                      alt={img.alt}
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  )}
                 </Box>
+                <Box
+                  className={`absolute inset-0 flex items-center justify-center transition-opacity duration-300 ease-in-out bg-black bg-opacity-50 ${
+                    img.image ? "opacity-0 hover:opacity-100" : ""
+                  }`}
+                  style={{ position: "absolute" }}
+                >
+                  <Box className="text-white font-semibold text-lg p-3">
+                    {img.name}
+                  </Box>
+                </Box>
+                {img.image && (
+                  <Box className="absolute right-1 top-1 bg-red-500 rounded-lg text-white py-1 px-5">
+                    Web
+                  </Box>
+                )}
               </Box>
-              <Box className="absolute right-1 top-1 bg-red-500 rounded-lg text-white py-1 px-5">
-                Web
-              </Box>
-            </Box>
+            </>
           ))}
         </Box>
       </Box>
